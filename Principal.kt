e113cfe72c7578d092e265d3d2983bd54bce53db
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2022-07-22 08:24:23 -0600

    Comentarios

--- Principal.kt
+++ Principal.kt
@@ -16,34 +16,41 @@ data class ItemData(
 // -----------------------
 
 fun main() {
-    val result = processList(listOf(25, "Hola", null, false))
+    val result = processList(listOf(25, "Hola", 10.20, false))
     println(result)
 }
 
 fun processList(inputList: List<Any?>?): List<ItemData>? {
+    //ArrayList inicializada como empty
     var resultado: ArrayList<ItemData>? = ArrayList<ItemData>()
 
+    //Caso null
     if(inputList == null){
         resultado = null
         return resultado
+        //Caso empty
     }else if(inputList.isEmpty()){
         return resultado
     }
 
+    //Contador de posiciones
     var contador = 0
     for(item in inputList){
+        //Evaluacion de tipo de entrada
         when(item){
             is String -> agregarString(resultado,contador,item)
             is Int -> agregarInt(resultado,contador,item)
             is Boolean -> agregarBoolean(resultado,contador,item)
             else -> if(item != null)agregarOtro(resultado,contador,item)
         }
+        //Incremento del contador
         contador++
     }
 
     return resultado
 }
 
+//Funcion para agregar strings
 fun agregarString(resultado:ArrayList<ItemData>?,pos:Int,value:String):Unit {
     val newItem = ItemData(
         originalPos = pos,
@@ -54,6 +61,7 @@ fun agregarString(resultado:ArrayList<ItemData>?,pos:Int,value:String):Unit {
     resultado?.add(newItem)
 }
 
+//Funcion para agregar enteros
 fun agregarInt(resultado:ArrayList<ItemData>?,pos:Int,value:Int):Unit {
     val newItem = ItemData(
         originalPos = pos,
@@ -64,6 +72,7 @@ fun agregarInt(resultado:ArrayList<ItemData>?,pos:Int,value:Int):Unit {
     resultado?.add(newItem)
 }
 
+//Funcion para agregar booleanos
 fun agregarBoolean(resultado:ArrayList<ItemData>?,pos:Int,value:Boolean):Unit {
     val newItem = ItemData(
         originalPos = pos,
@@ -74,6 +83,7 @@ fun agregarBoolean(resultado:ArrayList<ItemData>?,pos:Int,value:Boolean):Unit {
     resultado?.add(newItem)
 }
 
+//Funcion para agregar otros
 fun agregarOtro(resultado:ArrayList<ItemData>?,pos:Int,value:Any):Unit {
     val newItem = ItemData(
         originalPos = pos,
